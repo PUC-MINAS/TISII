ab5517dd4cfdca1a02820090e7071480d3575aab
raviassis <ravi.g.assis@gmail.com>
2018-05-14 10:31:12 -0300

    HTTPServer - Requisição create book

--- src/HTTPServer.java
+++ src/HTTPServer.java
@@ -4,9 +4,11 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.URI;
 
+import org.json.JSONObject;
 import org.simpleframework.http.Query;
 import org.simpleframework.http.Request;
 import org.simpleframework.http.Response;
+import org.simpleframework.http.Status;
 import org.simpleframework.http.core.Container;
 import org.simpleframework.http.core.ContainerSocketProcessor;
 import org.simpleframework.transport.connect.Connection;
@@ -43,13 +45,43 @@ public class HTTPServer implements Container{
 			String method = request.getMethod();
 			String mensagem;
 			
-			
+			if ( path.startsWith("/services/book/create") ) {
+				// http://127.0.0.1:880/services/book/create
+				mensagem = ServiceBook.create(request);
+				//this.enviaResposta(Status.CREATED, response, mensagem);
+			}
+			else {
+				this.notFind(response, path);
+			}
 			
 			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void notFind(Response response, String path) throws Exception {
+		JSONObject error = new JSONObject();
+		error.put("error", "Not find.");
+		error.put("path", path);
+		sendResponse(Status.NOT_FOUND, response, error.toString());
+	}
+	
+	private void sendResponse(Status status, Response response, String str) throws Exception {
+
+		PrintStream body = response.getPrintStream();
+		long time = System.currentTimeMillis();
+
+		response.setValue("Content-Type", "application/json");
+		response.setValue("Server", "Controle de estoqueService (1.0)");
+		response.setDate("Date", time);
+		response.setDate("Last-Modified", time);
+		response.setStatus(status);
+
+		if (str != null)
+			body.println(str);
+		body.close();
 	}
 
 }
---
+++ src/ServiceBook.java
@@ -0,0 +1,10 @@
+import org.simpleframework.http.Request;
+
+public class ServiceBook {
+
+	public static String create(Request request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
