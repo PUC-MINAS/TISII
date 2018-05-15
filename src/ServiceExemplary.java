f5b66a7d6fa5c3327475c76e1443b240fae7d0ec
raviassis <ravi.g.assis@gmail.com>
2018-05-15 15:23:18 -0300

    HTTPServer.java, cadastrarExemplar.html e ServiceExemplary.java
    HTTPServer tratando  requisição para cadastrar exemplar;
    Pagina Cadastrar Exemplar fazendo requisição ajax para o servidor
    Criado o ServiceExemplary. Não implementado ainda.

---
+++ src/ServiceExemplary.java
@@ -0,0 +1,10 @@
+import org.simpleframework.http.Request;
+
+public class ServiceExemplary {
+
+	public static boolean create(Request request) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
