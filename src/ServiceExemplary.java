f3556f55f66afd9bf35731db5722c088d7670135
raviassis <ravi.g.assis@gmail.com>
2018-05-21 09:03:00 -0300

    StatusExemplary

--- src/ServiceExemplary.java
+++ src/ServiceExemplary.java
@@ -2,6 +2,8 @@ import org.simpleframework.http.Request;
 
 public class ServiceExemplary {
 
+	private static Archive<Exemplary> exemplaries = new Archive<Exemplary>("Archives/exemplaries.json");
+	
 	public static boolean create(Request request) {
 		// TODO Auto-generated method stub
 		return true;
