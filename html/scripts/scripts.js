4ca6893ffb5384b21bffb8f3cd196b704ec4804a
raviassis <ravi.g.assis@gmail.com>
2018-05-27 02:16:31 -0300

    Pagina Cadastrar Usuário
    Formulario cadastro Usuário, e funcoes de requisição

--- html/scripts/scripts.js
+++ html/scripts/scripts.js
@@ -82,3 +82,73 @@ function fillSelectExemplary(){
 		}
 	});
 }
+
+function createUser(){
+	var elements = $("#formUser")[0].elements;
+	var formData = new FormData($("#formUser")[0]); 
+	$.ajax({
+		url : "http://127.0.0.1:781/services/user/create",
+	    type : 'POST',
+	    processData: false,
+  		contentType: false,
+    	data : formData,
+    	beforeSend : function(){
+      		
+    	}
+	}).done(function(response){
+		bootbox.alert({
+			message: "Cadastro realizado com sucesso",
+			size: "small"
+		});
+	}).fail(function(){
+		bootbox.alert({
+			message: "Não foi possível realizar o cadastro.",
+			size: "small"
+		});
+	});
+}
+
+
+function nextUserForm(){
+	var inputStep = $("#inputStep");
+	var steps = $('#steps');
+
+	if(inputStep.val() == "1"){
+		$("#step1").css('display', 'none');
+		$("#step2").css('display', 'block');
+		$("#btnBack").css('display', 'block');
+		inputStep.val('2');
+		steps.html("2 de 3");
+	}
+	else if (inputStep.val() == "2"){
+		$("#step2").css('display', 'none');
+		$("#step3").css('display', 'block');
+		$("#btnProx").html("Finalizar");
+		inputStep.val('3');
+		steps.html("3 de 3");
+	}
+	else if(inputStep.val() == "3"){
+		createUser();
+	}
+}
+
+function backUserForm(){
+	var inputStep = $("#inputStep");
+	var steps = $('#steps');
+
+	if(inputStep.val() == "2"){
+		$("#step2").css('display', 'none');
+		$("#step1").css('display', 'block');
+		$("#btnBack").css('display', 'none');
+		inputStep.val('1');
+		steps.html("1 de 3");
+	}
+	else if (inputStep.val() == "3"){
+		$("#step3").css('display', 'none');
+		$("#step2").css('display', 'block');
+		$("#btnProx").html("Próximo");
+		inputStep.val('2');
+		steps.html("2 de 3");
+	}
+
+}
