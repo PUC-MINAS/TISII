d1e5009ddcb5b422da6a3bdf0f79181428d8c840
raviassis <ravi.g.assis@gmail.com>
2018-05-28 01:04:59 -0300

    Criação de classes
    Criado as classes:
    Loan
    StatusUser
    TypeUser

--- src/ServiceUser.java
+++ src/ServiceUser.java
@@ -2,7 +2,7 @@ import org.simpleframework.http.Request;
 
 public class ServiceUser {
 	
-	
+	// Not implemented yet
 	public static boolean create(Request request) {
 		// TODO Auto-generated method stub
 		return false;
