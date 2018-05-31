51490db936b47e54c33bd7a6a3dd31caf621b2c8
Jonathas Leandro <jonathasbent@gmail.com>
2018-05-31 15:35:56 -0300

    ServiceBook.delete

--- src/ServiceBook.java
+++ src/ServiceBook.java
@@ -10,8 +10,9 @@ public class ServiceBook {
 	
 	private static Archive<Book> books = new Archive<Book>("Archives/books.json");
 
+	//Cheks if the book exists, if not creates and return true, if does return false
 	public static boolean create(Request request) {
-		Query query = request.getQuery();
+//		Query query = request.getQuery();
 		
 		//Creates a book
 		Book b = new Book();
@@ -26,10 +27,7 @@ public class ServiceBook {
 		
 		//Checks if the book already exists
 		List<Book> list = books.read();
-		boolean found = false;
-		for(int i=0;i<list.size() && !found;i++)
-			if(b.equals(i))
-				found=true;
+		boolean found = list.contains(b);
 		
 		//if book doesn't exists, creates a book
 		boolean created=false;
@@ -41,11 +39,37 @@ public class ServiceBook {
 		return created;
 	}
 
+	//returns a list of Books
 	public static List<Book> read(Request request) {
-		Query query = request.getQuery();
+//		Query query = request.getQuery();
 		List<Book> list = books.read();
-
 		return list;
 	}
-
+	
+	//Cheks if a book exist, if not returns false, if does deletes and return true 
+	public static boolean delete(Request request){
+		//Creates a book
+		Book b = new Book();
+		b.setName(request.getParameter("name"));
+		b.setPublishingCompany(request.getParameter("publishingCompany"));
+		b.setLanguage(request.getParameter("language"));
+		b.setIsbn(Long.parseLong(request.getParameter("isbn")));
+		b.setGenre(Integer.parseInt(request.getParameter("genre")));
+		b.setSynopsis(request.getParameter("synopsis"));
+		b.setAuthor(request.getParameter("Author"));
+		b.setSynopsis(request.getParameter("synopsis"));
+		
+		//Checks if the book exists 
+		List<Book> list = books.read();
+		int index = list.indexOf(b); 
+				
+		//deletes the book
+		boolean deleted=false;
+		if(index>=0) {
+			list.remove(index);
+			deleted=true;
+		}
+			
+		return deleted;
+	}
 }
